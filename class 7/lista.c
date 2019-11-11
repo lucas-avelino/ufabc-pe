50697fa41954fbb8d3c994cf625e19e5b5908385
Lucas Avelino <lucas.avelino1203@gmail.com>
2019-11-11 16:03:56 -0300

    added: class 7 tasks 4,10/10,00

--- class 7/lista.c
+++ class 7/lista.c
@@ -17,63 +17,62 @@
 #include "item.h"
 #include "lista.h"
 
-no *cria(item dado) {
+no *cria(item dado)
+{
+    no* p = malloc(sizeof(no));
+    *p = (no) {dado, NULL};
 
-
-
-      // ... seu código aqui.
-
-
-  
-}
-  
-void exclui(no *x) {
-
-
-
-      // ... seu código aqui.
-
-
-  
+    return p;
 }
 
-void imprime_lista(no *cabeca) {
-  no *tmp = cabeca;
-  
-  while (tmp != NULL) {
-    imprime(tmp->dado);
-    tmp = tmp->prox;
-  }
-
-  printf("\n");
+void exclui(no *x)
+{
+    free(x);
 }
 
-no *busca(no *cabeca, item dado) {
+void imprime_lista(no *cabeca)
+{
+    no *tmp = cabeca;
 
+    while (tmp != NULL)
+    {
+        imprime(tmp->dado);
+        tmp = tmp->prox;
+    }
 
-
-      // ... seu código aqui.
-
-
-
+    printf("\n");
 }
 
-no *busca_rec(no *cabeca, item dado) {
-
-
-
-      // ... seu código aqui.
-
-
-
+no *busca(no *cabeca, item dado)
+{
+    if(cabeca->dado == dado){
+        return cabeca;
+    }
+    while(cabeca != NULL && cabeca->prox != NULL){
+        cabeca = cabeca->prox;
+        if(cabeca->dado == dado){
+            return cabeca;
+        }
+    }
+    return NULL;
 }
 
-no *final(no *cabeca) {
+no *busca_rec(no *cabeca, item dado)
+{
+    if(cabeca == NULL)
+        return NULL;
+    if(cabeca->dado == dado)
+        return cabeca;
+    return busca_rec(cabeca->prox, dado);
+}
 
-
-
-      // ... seu código aqui.
-
-
-
+no * final(no *cabeca)
+{
+    if(cabeca == NULL){
+        return cabeca;
+    }
+    while(cabeca->prox != NULL){
+        cabeca = cabeca->prox;
+    }
+    return cabeca;
 }
--- class 7/main.c
+++ class 7/main.c
@@ -17,133 +17,155 @@
 #include "item.h"
 #include "lista.h"
 
-// Lê da entrada padrão uma lista de números e 
+// Lê da entrada padrão uma lista de números e
 // popula uma lista ligada com esses números.
-no *le_lista_inicial() {
-  int n;
+no *le_lista_inicial()
+{
+    int n;
 
-  scanf("%d", &n);
+    scanf("%d", &n);
 
-  no *lista = NULL;
-  no *ultimo = NULL;
+    no *lista = NULL;
+    no *ultimo = NULL;
 
-  for (int i = 0; i < n; i ++) {
-    item dado;
-    scanf("%d", &dado);
+    for (int i = 0; i < n; i++)
+    {
+        item dado;
+        scanf("%d", &dado);
 
-    no *x = cria(dado);
+        no *x = cria(dado);
 
-    if (lista == NULL) 
-      lista = x;
-    else
-      ultimo->prox = x;
-    ultimo = x;
-  }
+        if (lista == NULL)
+            lista = x;
+        else
+            ultimo->prox = x;
+        ultimo = x;
+    }
 
-  return lista;
+    return lista;
 }
 
+int main()
+{
+    char operacao[20];
+    no *lista = le_lista_inicial();
 
-int main() {
-  char operacao[20];
-  no *lista = le_lista_inicial();  
-  
-  while (scanf("%s", operacao) == 1) {
-    if (strcmp(operacao, "is_empty") == 0) {
-      // imprime "TRUE\n" se a lista estiver vazia
-      // e "FALSE\n" caso contrário
+    while (scanf("%s", operacao) == 1)
+    {
+        if (strcmp(operacao, "is_empty") == 0)
+        {
+            // imprime "TRUE\n" se a lista estiver vazia
+            // e "FALSE\n" caso contrário
+            if (lista == NULL)
+            {
+                printf("TRUE\n");
+            }
+            else
+            {
+                printf("FALSE\n");
+            }
+        }
+        else if (strcmp(operacao, "push_front") == 0)
+        {
+            // lê um item x, cria novo nó com dado = x e
+            // insere nó criado no início da lista
+            item dado;
+            scanf("%d", &dado);
 
+            no *novoDado = cria(dado);
+            novoDado->prox = lista;
+            lista = novoDado;
+        }
+        else if (strcmp(operacao, "push_back") == 0)
+        {
+            // lê um item x, cria novo nó com dado = x e
+            // insere nó criado no final da lista
+            item dado;
+            scanf("%d", &dado);
+            no *novoDado = cria(dado);
+            no *ultimoDado = final(lista);
+            if (lista == NULL)
+            {
+                lista = novoDado;
+            }
+            else
+            {
+                printf("%d\n", ultimoDado->dado);
+                ultimoDado->prox = novoDado;
+            }
+        }
+        else if (strcmp(operacao, "pop_front") == 0)
+        {
+            // remove o primeiro nó da lista, imprime o
+            // campo dado (seu item) na saída padrão e
+            // exclui o nó removido
+            // imprime "IMPOSSIBLE\n" se a lista estiver vazia
+            no* toRemove = lista;
+            if (lista == NULL)
+            {
+                printf("IMPOSSIBLE\n");
+            }
+            else
+            {
+                lista = lista->prox;
+                printf("%d\n", toRemove->dado);
+                exclui(toRemove);
+            }
+        }
+        else if (strcmp(operacao, "pop_back") == 0)
+        {
+            // remove o último nó da lista, imprime o
+            // campo dado (seu item) na saída padrão e
+            // exclui o nó removido
+            // imprime "IMPOSSIBLE\n" se a lista estiver vazia
 
+            no* toRemove = final(lista);
 
-      // ... seu código aqui.
+            if (lista == NULL)
+            {
+                printf("IMPOSSIBLE\n");
+            }
+            else
+            {
+                printf("%d\n", toRemove->dado);
+                exclui(toRemove);
+            }
+        }
+        else if (strcmp(operacao, "print") == 0)
+        {
+            // imprime os itens na lista na ordem
+            // da esquerda para a direita
+            // imprime só o '\n' se a lista estiver vazia
 
+            imprime_lista(lista);
+        }
+        else if (strcmp(operacao, "search") == 0)
+        {
+            // lê um item x e busca por x na lista
+            // imprime -1 se x não for encontrado ou
+            // o índice do nó que contem o dado x
+            // (0 se for 1º nó, 1 se for o segundo, etc.)
+            // sempre imprimir o '\n' no final
+            item dado;
+            scanf("%d\n", &dado);
 
+            
 
-    } else if (strcmp(operacao, "push_front") == 0) {
-      // lê um item x, cria novo nó com dado = x e
-      // insere nó criado no início da lista
-      item dado;
-      scanf("%d", &dado);
+            // ... seu código aqui.
+        }
+        else if (strcmp(operacao, "clear") == 0)
+        {
+            // exclui todos os nós da lista, deixando-a vazia
 
+            // ... seu código aqui.
+        }
+        else
+        {
+            // Nesse caso a operação lida não é válida
 
-
-      // ... seu código aqui.
-
-
-
-    } else if (strcmp(operacao, "push_back") == 0) {
-      // lê um item x, cria novo nó com dado = x e
-      // insere nó criado no final da lista
-      item dado;
-      scanf("%d", &dado);
-
-
-
-      // ... seu código aqui.
-
-
-      
-    } else if (strcmp(operacao, "pop_front") == 0) {
-      // remove o primeiro nó da lista, imprime o
-      // campo dado (seu item) na saída padrão e
-      // exclui o nó removido
-      // imprime "IMPOSSIBLE\n" se a lista estiver vazia
-
-
-
-      // ... seu código aqui.
-
-
-
-    } else if (strcmp(operacao, "pop_back") == 0) {
-      // remove o último nó da lista, imprime o
-      // campo dado (seu item) na saída padrão e
-      // exclui o nó removido
-      // imprime "IMPOSSIBLE\n" se a lista estiver vazia
-
-
-
-      // ... seu código aqui.
-
-
-      
-    } else if (strcmp(operacao, "print") == 0) {
-      // imprime os itens na lista na ordem
-      // da esquerda para a direita
-      // imprime só o '\n' se a lista estiver vazia
-
-      imprime_lista(lista);
-      
-    } else if (strcmp(operacao, "search") == 0) {
-      // lê um item x e busca por x na lista
-      // imprime -1 se x não for encontrado ou
-      // o índice do nó que contem o dado x
-      // (0 se for 1º nó, 1 se for o segundo, etc.)
-      // sempre imprimir o '\n' no final
-      item dado;
-      scanf("%d", &dado);
-
-
-
-      // ... seu código aqui.
-
-
-      
-    } else if (strcmp(operacao, "clear") == 0) {
-      // exclui todos os nós da lista, deixando-a vazia
-
-
-
-      // ... seu código aqui.
-
-
-      
-    } else {
-      // Nesse caso a operação lida não é válida
-      
-      printf("INVALID OPERATION\n");
+            printf("INVALID OPERATION\n");
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
