7244aad8600790440bffd9d38ee2a4349b1f9d7e
Lucas Avelino <lucas.avelino1203@gmail.com>
2019-12-06 20:33:55 -0300

    added: bonus 1 multiplicação

--- bonus 1/main.c
+++ bonus 1/main.c
@@ -20,10 +20,14 @@ void pushBack(Node **head, Node *item);
 void pushFront(Node **head, Node *item);
 void imprimeLista(Node *cabeca);
 void inverte(Node **cabeca);
+int length(Node *cabeca);
+Node *getElement(Node *list, int index);
 
 //Logica
 Node *soma(Node *num1, Node *num2);
-Node *MULTIPLICA(Node *num1, Node *num2);
+Node *multiplica(Node *num1, Node *num2);
+Node *multiplicaEscalar(Node *num1, int escalar);
+int length(Node *num1);
 
 void main()
 {
@@ -57,6 +61,7 @@ void main()
     // inverte(&numero1);
     // inverte(&numero2);
     imprimeLista(soma(numero1, numero2));
+    imprimeLista(multiplica(numero1, numero2));
     // imprimeLista(numero1);
     // imprimeLista(numero2);
 }
@@ -68,8 +73,8 @@ Node *soma(Node *num1, Node *num2)
     while (num1 != NULL && num2 != NULL)
     {
         int soma = (num1->dado + num2->dado + toAdd);
-        int resto = soma%10;
-        toAdd = (soma - resto)/10;
+        int resto = soma % 10;
+        toAdd = (soma - resto) / 10;
         // Node * resto = cria(resto);
         pushFront(&resultado, cria(resto));
         num1 = num1->prox;
@@ -77,29 +82,66 @@ Node *soma(Node *num1, Node *num2)
     }
     if (num1 == NULL)
     {
-        for (; num2 != NULL; num2 = num2->prox){
+        for (; num2 != NULL; num2 = num2->prox)
+        {
             int soma = (num2->dado + toAdd);
             int resto = soma % 10;
-            toAdd = (soma - resto)/10;
+            toAdd = (soma - resto) / 10;
             pushFront(&resultado, cria(resto));
-            
         }
-        
     }
     else if (num2 == NULL)
     {
-        for (; num1 != NULL; num1 = num1->prox){
+        for (; num1 != NULL; num1 = num1->prox)
+        {
             int soma = (num1->dado + toAdd);
             int resto = soma % 10;
-            toAdd = (soma - resto)/10;
+            toAdd = (soma - resto) / 10;
             pushFront(&resultado, cria(resto));
-            
         }
     }
-    if(num1 == NULL && num2 == NULL && toAdd > 0){
+    if (num1 == NULL && num2 == NULL && toAdd > 0)
+    {
         pushFront(&resultado, cria(toAdd));
     }
     return resultado;
+}
+
+Node *multiplica(Node *num1, Node *num2)
+{
+    int tamanho = length(num2);
+    Node **paraSomar = malloc(tamanho * sizeof(Node));
+
+    printf("117: %d\n", getElement(num2, 3)->dado);
+    for (int i = 0; i < tamanho && num2!=NULL; i++)
+    {
+        paraSomar[i] = multiplicaEscalar(num1, getElement(num2, i)->dado);
+        for (int j = 0; j < i; j++)
+        {
+            pushBack(&paraSomar[i], cria(0));
+        }
+        num2 = num2->prox;
+    }
+    Node 
+    for (int i = 0; i < tamanho; i++)
+    {
+       imprimeLista(paraSomar[i]);
+    }
+    
+    return paraSomar[3];
+}
+
+Node *multiplicaEscalar(Node *num1, int escalar)
+{
+    int tamanho = length(num1);
+    Node *no = NULL;
+    for (int i = 0; i < tamanho && num1!=NULL; i++)
+    {
+        pushFront(&no, cria(escalar * (getElement(num1, i)->dado)));
+        num1 = num1->prox;
+    }
+    
+    return no;
 }
 
 //Biblioteca
@@ -174,4 +216,24 @@ void inverte(Node **cabeca)
         m = t;
     } while (t != NULL);
     antigoIncio->prox = NULL;
+}
+
+int length(Node *cabeca)
+{
+    int i;
+    if(cabeca == NULL) return 0;
+    for (i = 1; cabeca != NULL; cabeca = cabeca->prox)
+        i++;
+    return i;
+}
+
+Node *getElement(Node *list, int index)
+{
+    int i;
+    Node * e = list;
+    for (i = 0; i < index && e != NULL; i++)
+    {
+        e = e->prox;
+    }
+    return list;
 }
